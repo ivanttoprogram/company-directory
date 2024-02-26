b9b7041ac9a83c505cc4e07c79a5fb411611c064
Cheryl Lopez <clopez51@stu.southtexascollege.edu>
2024-02-25 22:18:53 -0600

    login successful

--- src/composables/useAuth.js
+++ src/composables/useAuth.js
@@ -1,25 +1,24 @@
 import router from '../router'
 
 import { firebaseApp } from './useFirebase'
-import { getAuth, signInWithEmailandPassword, signOut } from 'firebase/auth'
+import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth'
 import { useAuth as firebaseAuth } from '@vueuse/firebase/useAuth'
 
-const auth = getAuth( firebaseApp )
+const auth = getAuth(firebaseApp)
 
-const { isAuthenticated, user } = firebaseApp(auth)
+const { isAuthenticated, user } = firebaseAuth(auth)
 
 export const useAuth = () => {
     const login = async (username, password) => {
-        await signInWithEmailandPassword(auth, username, password)
+        await signInWithEmailAndPassword(auth, username, password)
         return isAuthenticated.value
     }
 
-
     const logout = async () => {
         await signOut(auth)
-        router.puah({ name: 'Home' })
+        router.push({ name: 'Home'})
     }
 
-    return { isAuthenticated, user, login, logout }
+    return {isAuthenticated, user, login, logout}
 
 }
