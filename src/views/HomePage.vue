a87b570d97236e5fe7e4cdc1c4a28d4399b88be5
Cheryl Lopez <clopez51@stu.southtexascollege.edu>
2024-02-25 23:28:03 -0600

    changed bg color

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,6 +2,7 @@
 //import { ref } from 'vue'
 //const title = ref('Hello There ✋')
 import MainCards from '@/components/MainCards.vue';
+import MainPagination from '@/components/MainPagination.vue';
 </script>
 
 <template>
